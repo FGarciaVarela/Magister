cc828567e807835cdfe7d107dab996d025df6f24
FGarciaVarela <pancho.garcia@uc.cl>
2024-03-30 18:17:30 -0300

    probando cambios

---
+++ pagWeb/src/init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Descargando el modelo en_core_web_trf para spaCy..."
+python3 -m spacy download en_core_web_trf
+
+sleep 5
+
+echo "Iniciando servidor FastAPI con Uvicorn..."
+uvicorn main:app --reload
