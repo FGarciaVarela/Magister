9d9837a6b6869a8f6f2525b6b2376826fd72f390
FGarciaVarela <pancho.garcia@uc.cl>
2024-03-31 10:17:44 -0300

    cambio de puerto

--- pagWeb/src/main.py
+++ pagWeb/src/main.py
@@ -77,4 +77,4 @@ async def detect_abstract_words(item: Text):
     return {"abstractWords": list(palabras_abstractas_encontradas)}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
